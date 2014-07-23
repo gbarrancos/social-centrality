9bf51fb98bb7c4b923cc3d349cb995a344e82cec
Gustavo Barrancos <gbarrancos@gmail.com>
2014-07-23 13:47:16 -0300

    retrieving mutual friends from facebook api

--- src/social_centrality/facebook.clj
+++ src/social_centrality/facebook.clj
@@ -3,17 +3,19 @@
             [clojure.data.json :as json]))
 
 (def friends-uri "https://graph.facebook.com/me/friends")
-(def mutual-friends-uri "")
+
+(defn user-uri [userid]
+  (str "https://graph.facebook.com/" userid))
 
 
-
-(defn auth-request [uri user-token]
-  (json/read-str
-   (:body (http/get uri {:query-params {"access_token" user-token}}))))
+(defn auth-get [uri user-token params]
+  (let [query-pars (assoc params "access_token" user-token)]
+    (json/read-str
+     (:body (http/get uri {:query-params query-pars })))))
 
 
 (defn my-friends [user-token]
-  (loop [resp (auth-request friends-uri user-token)
+  (loop [resp (auth-get friends-uri user-token {})
          result []]
     (let [friends (concat result (get resp "data"))
           next-page (get-in resp ["paging" "next"])]
@@ -22,13 +24,22 @@
         (recur (json/read-str (:body (http/get next-page)))
                friends)))))
 
-  
-  
-  
-  
+
+(defn paging-for [cursors-map]
+  (let [before (get cursors-map "before")
+        after (get cursors-map "after")]
+    (if (= before after) nil after)))
 
 
-
-(defn mutual-friends-with [userid user-token])
-
+(defn mutual-friends-with [userid user-token]
+  (loop [pars {"fields" "context.fields(mutual_friends)"}
+         result []]
+    (let [resp (get-in (auth-get (user-uri userid) user-token pars)
+                       ["context" "mutual_friends"])
+          mut-friends (concat result (get resp "data"))
+          next-cursor (paging-for (get-in resp ["paging" "cursors"]))]
+      (if-not next-cursor
+        mut-friends
+        (recur (assoc pars "after" next-cursor)
+               mut-friends)))))
   
--- test/social_centrality/facebook_test.clj
+++ test/social_centrality/facebook_test.clj
@@ -3,6 +3,8 @@
             [social-centrality.facebook :refer :all])
   (:use clj-http.fake))
 
+;TODO: extract these JSON strings to individual files
+
 (def my-friends-1 "{\"data\":[{\"name\":\"Ishida Uryuu\",\"id\":\"100\"},{\"name\":\"Urahara Kisuke\",\"id\":\"500\"}],\"paging\":{\"next\":\"https://graph.facebook.com/generatedlink\"}}")
 
 (def my-friends-2 "{ \"data\": [{\"name\":\"Inoue Orihime\",\"id\":\"900\"}], \"paging\": {\"previous\": \"https://graph.facebook.com/v2.0/274375839421076/friends?limit=5000&offset=0&access_token=CAAHeLWh2UZBUBAG7ZA1oBBY4ZBBEDn74xZBAflDkf5v6lmCmfdyjscNE0XVGMi6u0bZAHvwXPXTQOp6pNIxIviO94vDCRFBhb6aAFzaTYw6IUtl5DbI72KZBxZAIXIgJqzsONc1H5KdZCc2BcXfrZCXG0s2qfzZAXfEQc8BKc5mtKd3qVWjMnjZACaZB5ri6u9IpZAVmul8ss73qCD2L64ZCgDiZAzT\"}}")
@@ -16,7 +18,30 @@
         "https://graph.facebook.com/generatedlink"
         (fn [req] {:status 200 :headers {} :body my-friends-2})}
 
-      (is (= '(("Ishida Uryuu" "100")
-               ("Urahara Kisuke" "500")
-               ("Inoue Orihime" "900"))
-             (map vals (my-friends "atoken")))))))
+      (is (= [{"name" "Ishida Uryuu", "id" "100"}
+              {"name" "Urahara Kisuke", "id" "500"}
+              {"name" "Inoue Orihime", "id" "900"}] (vec(my-friends "atoken")))))))
+
+
+
+(def mut-friends-1 "
+{\"context\":{\"mutual_friends\":{\"data\":[{\"id\": \"255\", \"name\": \"Ikkaku Madarame\"} {\"id\":\"69\", \"name\": \"Kensei Muguruma\"}], \"paging\": {\"cursors\": {\"before\": \"b4\", \"after\": \"2\"}}, \"summary\": {\"total_count\": 3 }}}, \"id\": \"500\"}")
+
+(def mut-friends-2 "
+{\"context\":{\"mutual_friends\":{\"data\":[{\"id\": \"444\", \"name\": \"Shuhei Hisagi\"}], \"paging\": {\"cursors\": {\"before\": \"last\", \"after\": \"last\"}}, \"summary\": {\"total_count\": 3 }}}, \"id\": \"1389619557960841\"}")
+
+
+
+(deftest mutual-friends-with-user-test
+  (testing "Retrieving mutual friends list from Facebook Graph API"
+    (with-fake-routes
+      { "https://graph.facebook.com/500?fields=context.fields%28mutual_friends%29&access_token=atoken"
+        (fn [req] {:status 200 :headers {} :body mut-friends-1})
+        "https://graph.facebook.com/500?fields=context.fields%28mutual_friends%29&access_token=atoken&after=2"
+        (fn [req] {:status 200 :headers {} :body mut-friends-2})
+       }
+
+      (is (= [{"id" "255", "name" "Ikkaku Madarame"}
+              {"id" "69", "name" "Kensei Muguruma"}
+              {"id" "444", "name" "Shuhei Hisagi"}] (vec (mutual-friends-with "500" "atoken")))))))
+  
